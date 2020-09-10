6afe29bd6d90d6971ca2751b1a271fca7d9e6e52
canelle20214 <canelledelbreil@gmail.com>
2020-09-10 10:01:47 +0200

    supervisor

--- lib/chat.ex
+++ lib/chat.ex
@@ -1,56 +1,47 @@
 defmodule Chat do
   use GenServer
-  alias Chat.Registry, as: Regi
-
-  @registry :chatregistry
+  require Logger
 
 
-  def start_link(_) do
-    {ok, _} = Regi.start_link()
-    GenServer.start_link(__MODULE__, [], [name: :chat]) #{:via, Chat.Registry, {:where, name} })
+  def start_link(opt) do
+    GenServer.start_link(__MODULE__, [], opt)
   end
 
   def init(_) do
-     {:ok, []}
+    {:ok, []}
   end
 
 
-
-  def add_message(message) do
-    Registry.dispatch(Chat.Registry, @registry, fn entries ->
-      for {pid, _} <- entries, do: GenServer.cast(pid, {:add_message, message}) #send(pid, {:send_message, message})
+  def send_message(pid, message) do
+      list = Supervisor.which_children(Sup)
+      children = Enum.filter(list, fn {_id, child, _type, module} ->
+      module == [__MODULE__] && child !== pid
+    end)
+    Enum.each(children, fn {_id, child, _type, _modules} ->
+      GenServer.cast(child, {:send_message, message, child})
     end)
   end
 
-  def send_message(message) do
-    Registry.dispatch(Chat.Registry, @registry, fn entries ->
-      for {pid, _} <- entries, do: GenServer.cast(pid, {:send_message, message}) #send(pid, {:send_message, message})
-    end)
-    # GenServer.cast(:chat, {:send_message, message})
+  def add_message(pid, message) do
+    GenServer.cast(pid, {:add_message, message})
   end
 
-  def get_messages do
-    GenServer.call(:chat, :get_messages)
+  def get_messages(pid) do
+    GenServer.call(pid, :get_messages)
   end
-
-
 
 
   def handle_call(:get_messages, _from, state) do
-    {:reply, state, []}
+    {:reply, Enum.reverse(state), state}
   end
 
-  def handle_cast(_pid, {:send_message, message}, state) do
-    add_message(message)
-    {:noreply, state}
-  end
-
-  def handle_cast(pid, {:add_message, _message}, state) when pid === self() do
-    {:noreply, state}
-  end
-
-  def handle_cast(_pid, {:add_message, message}, state) do
+  def handle_cast({:add_message, message}, state) do
     {:noreply, [ message | state ]}
+  end
+
+  def handle_cast({:send_message, message, to}, state) do
+    Chat.add_message(to, message)
+    {:noreply, state}
   end
 
 end
--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -1,9 +1,17 @@
 defmodule Chat.Supervisor do
   use Application
 
+
   def start(_type, _arg) do
-    children = [ Chat ]
-    Supervisor.start_link(children, [strategy: :one_for_one])
+    children = [
+      %{:id => :primary,
+      :start => {Chat, :start_link, [[]]}
+    }
+    ]
+    Supervisor.start_link(children, [strategy: :one_for_one, name: Sup])
   end
 
+  def start_child(module, id) do
+    Supervisor.start_child(Sup, %{:id => id, :start => {module, :start_link, [[]]}})
+  end
 end
