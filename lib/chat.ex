394538fc50538d8f955a97c32ce79e498b4b7757
canelle20214 <canelledelbreil@gmail.com>
2020-09-15 12:28:06 +0200

    chat scalable

--- lib/chat.ex
+++ lib/chat.ex
@@ -18,14 +18,24 @@ defmodule Chat do
     {:ok, %State{socket: socket}}
   end
 
-  def handle_info({:tcp, socket, data}, %State{name: nil} = state) do
-    name = String.replace(data, "\r\n", "")
-    newstate = struct(State, [name: name, socket: state.socket])
-    :gen_tcp.send(state.socket, "Nice to meet you #{newstate.name}\r\n")
+
+  def handle_info({:tcp_closed, socket}, state) do
+    Logger.info "#{inspect socket} just closed"
+    {:stop, :normal, state}
+  end
+
+  def handle_info({:tcp_error, _socket, reason}, state) do
+    Logger.error reason
+    {:stop, reason, state}
+  end
+
+  def handle_info({:tcp, _socket, data}, %State{name: nil} = state) do
+    newstate = struct(State, [name: String.trim(data), socket: state.socket])
+    :gen_tcp.send(state.socket, "Nice to meet you #{newstate.name} !\r\n")
     {:noreply, newstate}
   end
 
-  def handle_info({:tcp, socket, data}, state) do
+  def handle_info({:tcp, _socket, data}, state) do
     :pg2.get_members(:first_group)
     |> Enum.filter(fn n ->
       n !== self()
@@ -36,19 +46,37 @@ defmodule Chat do
     {:noreply, state}
   end
 
-  def handle_info({from, data}, state) do # when from !== socket
-    :gen_tcp.send(state.socket, "#{from.name} said: #{data}")
+  def handle_info({:disconnect, from}, state) do
+    :gen_tcp.send(state.socket, "#{from.name} quit the conversation. \r\n")
     {:noreply, state}
   end
 
-  def handle_info({:tcp_closed, socket}, state) do
-    Logger.info "#{socket} just closed"
+  def handle_info({from, data}, state) do # when from !== socket
+    :gen_tcp.send(state.socket, "#{from.name} said: #{data} \r\n")
+    {:noreply, state}
   end
 
-  def handle_info({:tcp_error, socket, reason}, state) do
-    Logger.error reason
+  def terminate({_reason, _term}, state) do
+    Logger.warn "#{state.name} quit the convo weirdly"
+    :ok
   end
 
+  def terminate(:normal, state) do
+    :pg2.get_members(:first_group)
+    |> Enum.filter(fn n ->
+      n !== self()
+    end)
+    |> Enum.each(fn n ->
+      send(n, {:disconnect, state})
+    end)
+    Logger.warn "#{state.name} quit the convo"
+    :ok
+  end
+
+  def terminate(:shutdown, state) do
+    Logger.warn "shutdown #{inspect state}"
+    :ok
+  end
 
 
   # :inet.setopts(socket, active: :once)
